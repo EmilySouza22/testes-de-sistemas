b86824a06ae4fa3330ff6a5adb578576e32a6ee5
EmilySouza22 <emilyseafonso@gmail.com>
2026-02-25 17:07:14 -0300

    Configurando repositório e adicionando arquivos Jest

---
+++ aula1/SA/frontend/jest.config.js
@@ -0,0 +1,8 @@
+//npm test mas ainda nao tem testes
+export default {
+  testEnvironment: 'jsdom',
+  transform: {},
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+};
